f79043d5af73814400df0d89cbf2c41c48bd87b1
Sam Penders <sam.penders10@gmail.com>
2023-11-29 13:58:35 -0600

    Create json if doesn't exist

--- database/feed_database.py
+++ database/feed_database.py
@@ -1,12 +1,15 @@
 import json
+import os
 
 JSON_NAME = "database/adversarial_prompts.json"
-def send_to_database(prompt="", expected="", llm_output="", notes=""):
+def send_to_database(prompt="", expected="", llm_output=""):
     new_entry = {}
     new_entry['prompt'] = prompt
     new_entry['expected_output'] = expected
     new_entry['llm_output'] = llm_output
-    new_entry['notes'] = notes
+    if not os.path.exists(JSON_NAME):
+        with open(JSON_NAME, 'w') as fp:
+            fp.write('[]')
     with open(JSON_NAME, 'r+') as fp:
         data = json.load(fp)
         data.append(new_entry)
